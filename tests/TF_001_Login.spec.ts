ea6439644e88f2a399e084a5b091b4c86653a47b
jakobma <manfred.jakob@dataport.de>
2023-09-11 15:51:27 +0200

    renamed folder  pages to Pages

--- tests/TF_001_Login.spec.ts
+++ tests/TF_001_Login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/Login';
+import { LoginPage } from '../Pages/Login';
 
 
 test('Login Test', async ({ page }) => {
