a84c85a5ac64bb1557c48724d180bd904fbc2dfb
jakobma <manfred.jakob@dataport.de>
2023-09-11 21:34:20 +0200

    Verwendung von Steps

--- tests/TF_001_Login.spec.ts
+++ tests/TF_001_Login.spec.ts
@@ -1,14 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login';
 
+test.describe('Erfolgreicher Standard Login', () => {
+  test('Login Test', async ({ context, page }) => {
+    const server = 'portal';
+    const url = 'https://' + server + '.qs.schule-sh.de/univention/login/#/'; 
+    const Login = new LoginPage(page);
+    
+    await test.step('Hier passiert nichts', async () => {  
+    })
 
-test('Login Test', async ({ page }) => {
-  const server = 'portal';
-  const url = 'https://' + server + '.qs.schule-sh.de/univention/login/#/'; 
-  const Login = new LoginPage(page);
+    await test.step('Negativer login', async () => {
+      await Login.loginAndereURL('xxxxxxxx', 'xxxxxxx', url); 
+    })
+  })
+})
   
-  await Login.loginAndereURL('xxxxxxxx', 'xxxxxxx', url); 
 
-  
+
    //await page.pause();
-});
