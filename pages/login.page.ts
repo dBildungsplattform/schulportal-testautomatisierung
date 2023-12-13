18c5f9f696f9b3c15ec7f68da2c1069aec600ab2
Manfred <141728305+jakobmadp@users.noreply.github.com>
2023-12-13 07:50:19 +0100

    Neuer Testfall fertig, schedule action nur einmal die Stunde (#4)

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -2,23 +2,24 @@ import { expect, type Locator, Page } from '@playwright/test';
 
 export class LoginPage{
     readonly page: Page;
-    readonly text_h1_UeberschriftLoginSeite: Locator;
     readonly input_username: Locator;
     readonly input_password: Locator;
     readonly button_login: Locator;
     readonly button_logoff: Locator;
-    readonly text_h1_login: Locator;
+    readonly text_h1: Locator;
+    readonly text_span_inputerror: Locator;
 
     constructor(page){
         this.page = page;  
-        this.text_h1_login = page.getByRole('heading', { name: 'Sign in to your account' });
+        this.text_h1 = page.getByRole('heading', { name: 'Sign in to your account' });
         this.input_username = page.getByLabel('Username or email');
         this.input_password = page.getByLabel('Password');
-        this.button_login = page.getByRole('button', { name: 'Sign In' })
+        this.button_login = page.getByRole('button', { name: 'Sign In' });
+        this.text_span_inputerror = page.getByText('Invalid username or password.');
     }
 
     async login(username, password){
-        await expect(this.text_h1_login).toBeVisible();
+        await expect(this.text_h1).toBeVisible();
         await this.input_username.click();
         await this.input_username.fill(username);
         await this.input_password.click();
