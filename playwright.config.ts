c8998af7875a27ca2fbbe7cef78ddd7c38719ea1
jakobmadp~ <manfred.jakob@dataport.de>
2024-02-21 15:01:43 +0100

    env von lokal auf test.dev zurückgestellt

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from "dotenv";
 dotenv.config({
-  path: "./.env.local",
+  path: "./.env.dev",
 });
 
 export default defineConfig({
