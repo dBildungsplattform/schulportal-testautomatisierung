e0283e0e6e4483b8da3a89cf20859ce77b26ebdd
jakobmadp~ <manfred.jakob@dataport.de>
2024-05-02 12:08:20 +0200

    GET und POST in dem test api.spec

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,5 +1,5 @@
+import { faker } from '@faker-js/faker/locale/de';
 import { test, expect } from '@playwright/test';
-import { json } from '@data.json';
 import { LoginPage } from '../pages/LoginView.page';
 import { LandingPage } from '../pages/LandingView.page';
 import { StartPage } from '../pages/StartView.page';
@@ -9,10 +9,11 @@ const USER = process.env.USER;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 test.describe(`Spike um die API anzusprechen: Umgebung: ${process.env.UMGEBUNG}: URL: ${process.env.FRONTEND_URL}:`, () => {
-  test('GET', async ({ page, request,}) => {
+  test('GET und Post request', async ({ page}) => {
     const Login = new LoginPage(page);
     const Landing = new LandingPage(page);
     const Start = new StartPage(page);
+
     await test.step(`Anmelden mit Benutzer ${USER}`, async () => {
       await page.goto(FRONTEND_URL);
       await expect(Landing.text_Willkommen).toBeVisible();
@@ -21,25 +22,23 @@ test.describe(`Spike um die API anzusprechen: Umgebung: ${process.env.UMGEBUNG}:
       await expect(Start.text_h2_Ueberschrift).toBeVisible();
     })
 
-    await test.step(`API ansprechen mit GET ${USER}`, async () => {
+    await test.step(`GET Request personen, alle Benutzer lesen`, async () => {
       const response = await page.request.get('https://main.dev.spsh.dbildungsplattform.de/api/personen/'); 
-      console.log(await response.json());
+      expect(response.status()).toBe(200); 
     })
 
-
-    const allCookies = await page.context().cookies();
-    let mycookie = '';
-    allCookies.forEach((item) => {
-      if(item.name == 'connect.sid') {
-        mycookie = item.value;
-      }
-    })
+    await test.step(`POST Request personen, neuen Benutzer anlegen`, async () => {
+      const Vorname = 'TAutoV' + faker.person.firstName(); 
+      const Nachname = 'TAutoN' + faker.person.lastName() + '-' + faker.person.lastName(); // Wahrscheinlichkeit doppelter Namen verringern
+      const response = await page.request.post('https://main.dev.spsh.dbildungsplattform.de/api/personen/', {
+        data: { 
+          "name": {
+            "vorname": Vorname,
+            "familienname": Nachname
+          }
+        }
+      })   
+      expect(response.status()).toBe(201);
+    }) 
   })  
 })
-
-
-
-
-
-
-
