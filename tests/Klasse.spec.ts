c4679e35fe6074d53e76287d572c5b0434c20ef9
Manfred <141728305+jakobmadp@users.noreply.github.com>
2024-06-28 09:34:28 +0200

    spsh 686 Neue Testfälle Klassen fertig (#36)
    * Neue Testfälle Klassen fertig
    
    * Update pages/admin/KlasseCreationView.page.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update pages/admin/KlasseCreationView.page.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update pages/admin/KlasseManagementView.page.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update pages/admin/KlasseManagementView.page.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update tests/Klasse.spec.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update tests/Klasse.spec.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update tests/Klasse.spec.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>

---
+++ tests/Klasse.spec.ts
@@ -0,0 +1,79 @@
+import { test, expect } from "@playwright/test";
+import { LandingPage } from "../pages/LandingView.page";
+import { LoginPage } from "../pages/LoginView.page";
+import { StartPage } from "../pages/StartView.page";
+import { MenuPage } from "../pages/MenuBar.page";
+import { KlasseCreationViewPage } from "../pages/admin/KlasseCreationView.page";
+import { KlasseManagementViewPage } from "../pages/admin/KlasseManagementView.page";
+import { faker } from "@faker-js/faker/locale/de";
+
+const PW = process.env.PW;
+const ADMIN = process.env.USER;
+const FRONTEND_URL = process.env.FRONTEND_URL || "";
+
+test.describe(`Testfälle für die Administration von Klassen: Umgebung: ${process.env.UMGEBUNG}: URL: ${process.env.FRONTEND_URL}:`, () => {
+  test.beforeEach(async ({ page }) => {
+    await test.step(`Login`, async () => {
+      const Landing = new LandingPage(page);
+      const Startseite = new StartPage(page);
+      const Login = new LoginPage(page);
+
+      await page.goto(FRONTEND_URL);
+      await Landing.button_Anmelden.click();
+      await Login.login(ADMIN, PW);
+      await expect(Startseite.text_h2_Ueberschrift).toBeVisible();
+    });
+  });
+
+  test("Eine Klasse anlegen und die Klasse anschließend in der Ergebnisliste suchen", async ({ page }) => {
+    const Startseite = new StartPage(page);
+    const Menue = new MenuPage(page);
+    const KlasseCreationView = new KlasseCreationViewPage(page);
+    const KlasseManagementView = new KlasseManagementViewPage(page);
+    const SCHULNAME = "Testschule Schulportal";
+    const KLASSENNAME = "TAuto-PW-K-12 " + faker.lorem.word({ length: { min: 4, max: 8 }});
+
+    await test.step(`Dialog Schule anlegen öffnen`, async () => {
+      await Startseite.card_item_schulportal_administration.click();
+      await Menue.menueItem_KlasseAnlegen.click();
+      await expect(KlasseCreationView.text_h2_KlasseAnlegen).toHaveText("Neue Klasse hinzufügen");
+    });
+
+    await test.step(`Klasse anlegen`, async () => {
+      await KlasseCreationView.combobox_Schulstrukturknoten.click();
+      await page.getByText(SCHULNAME).click();
+      await KlasseCreationView.input_Klassenname.fill(KLASSENNAME);
+      await KlasseCreationView.button_KlasseAnlegen.click();
+      await expect(KlasseCreationView.text_success).toBeVisible();
+    });
+
+    await test.step(`In der Ergebnisliste prüfen, dass die neue Klasse angezeigt wird`, async () => {
+      await Menue.menueItem_AlleKlassenAnzeigen.click();
+      await KlasseManagementView.combobox_Filter_Schule.fill(SCHULNAME);    
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
+      await KlasseManagementView.combobox_Filter_Klasse.fill(KLASSENNAME);
+      await page.keyboard.press('ArrowDown');
+      await page.keyboard.press('Enter');
+      await expect(KlasseManagementView.text_h2_Klassenverwaltung).toHaveText("Klassenverwaltung");
+      await expect(page.getByRole("cell", { name: KLASSENNAME })).toBeVisible();
+    });
+  });
+
+  test("Ergebnisliste Klassen auf Vollständigkeit prüfen", async ({ page }) => {
+    const Startseite = new StartPage(page);
+    const Menue = new MenuPage(page);
+    const KlasseManagementView = new KlasseManagementViewPage(page);
+
+    await test.step(`Klassenverwaltung öffnen und Alle Elemente in der Ergebnisliste auf Existenz prüfen`, async () => {
+      await Startseite.card_item_schulportal_administration.click();
+      await Menue.menueItem_AlleKlassenAnzeigen.click();
+      await expect(KlasseManagementView.text_h1_Administrationsbereich).toBeVisible();
+      await expect(KlasseManagementView.text_h2_Klassenverwaltung).toHaveText("Klassenverwaltung");
+      await expect(KlasseManagementView.combobox_Filter_Schule).toBeVisible();
+      await expect(KlasseManagementView.combobox_Filter_Klasse).toBeVisible();
+      await expect(KlasseManagementView.table_header_Dienststellennummer).toBeVisible();
+      await expect(KlasseManagementView.table_header_Klassenname).toBeVisible();
+    });
+  });
+});
