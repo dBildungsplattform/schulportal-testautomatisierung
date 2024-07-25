b9de73ec4b82a5a33500e7623d8b83bd6dc601f7
jakobmadp~ <manfred.jakob@dataport.de>
2024-07-25 09:00:24 +0200

    fix testid button_KlasseAnlegen

--- pages/admin/KlasseCreationView.page.ts
+++ pages/admin/KlasseCreationView.page.ts
@@ -13,7 +13,7 @@ export class KlasseCreationViewPage{
         this.text_h2_KlasseAnlegen = page.getByTestId('layout-card-headline');
         this.combobox_Schulstrukturknoten = page.getByTestId('schule-select').locator('.v-input__control'); 
         this.input_Klassenname = page.getByTestId('klassenname-input').locator('input');
-        this.button_KlasseAnlegen = page.getByTestId('klasse-creation-form-create-button');
+        this.button_KlasseAnlegen = page.getByTestId('klasse-form-create-button');
         this.text_success = page.getByTestId('klasse-success-text');
     }
 }
