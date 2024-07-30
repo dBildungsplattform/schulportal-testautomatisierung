770a188fc719f85187c39a8b9e9627e06fc94613
jakobmadp~ <manfred.jakob@dataport.de>
2024-07-30 14:04:39 +0200

    Neuerr Testfall Schulportal-Adminsitration mit Testdaten via api

---
+++ base/testHelper.ts
@@ -0,0 +1,178 @@
+import { Page } from '@playwright/test';
+import {faker} from "@faker-js/faker/locale/de";
+import {LandingPage} from "../pages/LandingView.page";
+import {LoginPage} from "../pages/LoginView.page";
+import {HeaderPage} from "../pages/Header.page";
+
+const PW = process.env.PW;
+const ADMIN = process.env.USER;
+const FRONTEND_URL = process.env.FRONTEND_URL || "";
+
+export interface UserInfo {
+    username: string,
+    password: string,
+    rolleId: string,
+    organisationId: string,
+    personId: string;
+}
+
+export async function createOrganisation(page: Page, name: string): Promise<string> {
+    const response = await page.request.post(FRONTEND_URL + 'api/organisationen/', {
+        data: {
+            "administriertVon": null,
+            "zugehoerigZu": null,
+            "kennung": faker.string.numeric({length: 6}),
+            "name": name,
+            "namensergaenzung": null,
+            "kuerzel": null,
+            "typ": "SCHULE",
+            "traegerschaft": null
+        }
+    });
+    const json = await response.json();
+    return json.id;
+}
+
+export async function createRolle(page: Page, rollenArt: string, organisationId: string, rolleName?: string): Promise<string> {
+    const response = await page.request.post(FRONTEND_URL + 'api/rolle/', {
+        data: {
+            "name": rolleName,
+            "administeredBySchulstrukturknoten": organisationId,
+            "rollenart": rollenArt,
+            "merkmale": [],
+            "systemrechte": []
+        }
+    });
+    const json = await response.json();
+    await page.pause();
+    console.log('start');
+    console.log(rolleName);
+    console.log(organisationId);
+    console.log(rollenArt);
+    console.log(await json);
+    console.log('ende');
+    return json.id;
+}
+
+export async function addSPToRolle(page: Page, rolleId: string, idSP: string): Promise<void> {
+    const response = await page.request.post(FRONTEND_URL + `api/rolle/${rolleId}/serviceProviders`, {
+        data: {
+            "serviceProviderId": idSP,
+        }
+    });
+}
+
+export async function addSystemrechtToRolle(page: Page, rolleId: string, systemrecht: string): Promise<void> {
+    const response = await page.request.patch(FRONTEND_URL + `api/rolle/${rolleId}`, {
+        data: {
+            "systemRecht": systemrecht
+        }
+    });
+}
+
+export async function createUser(page: Page, familienname: string, vorname: string, organisationId: string, rolleId: string): Promise<UserInfo> {
+    const response = await page.request.post(FRONTEND_URL + 'api/personenkontext-workflow/', {
+        data: {
+            "familienname": familienname,
+            "vorname": vorname,
+            "organisationId": organisationId,
+            "rolleId": rolleId
+        }
+    });
+    const json = await response.json();
+    await page.pause();
+    //console.log(await response.json());
+    await page.pause();
+    return {
+        username: json.person.referrer,
+        password: json.person.startpasswort,
+        rolleId: rolleId,
+        organisationId: organisationId,
+        personId: json.person.id
+    }
+}
+
+export async function createBenutzerWithUserContext(page: Page, organisationName: string, rollenArt: string, familienname: string, vorname: string, idSP: string, rolleName: string): Promise<UserInfo> {
+    // Sich mit dem Admin-User anmelden, damit die API-Calls auch Authorized sind und funktionieren:
+    const Landing = new LandingPage(page);
+    const Login = new LoginPage(page);
+    const Header = new HeaderPage(page);
+
+    if (await Header.button_logout.isVisible()) { //Abmelden, falls noch ein Nutzer aus einem vorherigen Test angemeldet ist:
+        await Header.button_logout.click();
+    }
+    await page.goto(FRONTEND_URL);
+    await Landing.button_Anmelden.click();
+    await Login.login(ADMIN, PW);
+    // page.on('console', msg => {
+    //     console.log(msg.text());
+    // });
+
+    // API-Calls machen:
+    // const organisationId: string = await createOrganisation(page, organisationName);  --> Es gibt keinen Endpunkt Organisation-löschen, Daten aus seeding
+    const organisationId: string = await getOrganisationId(page, organisationName);
+   // console.log('orga: ' + organisationId);
+    const rolleId: string = await createRolle(page, rollenArt, organisationId, rolleName);
+    //console.log('rolle: ' + rolleId);
+    await addSPToRolle(page, rolleId, idSP);
+    //console.log(familienname);
+    //console.log(vorname);
+    const userInfo: UserInfo = await createUser(page, familienname, vorname, organisationId, rolleId);
+    // await addSystemrechtToRolle(page, rolleId, 'PERSONEN_VERWALTEN');
+    await Header.button_logout.click();
+    console.log(userInfo);
+    return userInfo;
+}
+
+export async function getSPId(page: Page, nameSP: string): Promise<string> {
+    // Service-Provider-Ids auslesen
+    const Landing = new LandingPage(page);
+    const Login = new LoginPage(page);
+    const Header = new HeaderPage(page);
+
+    await page.goto(FRONTEND_URL);
+    await Landing.button_Anmelden.click();
+    await Login.login(ADMIN, PW);
+    
+    let idSP = '';
+    const response_provider = await page.waitForResponse((response) =>
+        response.url().includes("/api/provider")
+    );
+
+    const responseBody_provider = await response_provider.json();
+    responseBody_provider.forEach((element) => {
+        if (element.name === nameSP) {
+            idSP = element.id
+        }
+    });
+    
+    await Header.button_logout.click();
+    return idSP;   
+}
+
+export async function getOrganisationId(page: Page, nameOrganisation: string): Promise<string> {
+    // Organisations-Id auslesen
+    const Landing = new LandingPage(page);
+    const Login = new LoginPage(page);
+    const Header = new HeaderPage(page);
+
+    await Header.button_logout.click();
+    await Landing.button_Anmelden.click();
+    await Login.login(ADMIN, PW);
+    
+    const response = await page.request.get(FRONTEND_URL + `api/organisationen?name=${nameOrganisation}`, {})   
+    const json = await response.json();
+    await Header.button_logout.click();
+   // console.log( await response.json());
+   // console.log('orga: ' + nameOrganisation);
+   // console.log('orga: ' + json[0].id); 
+    return json[0].id;
+}
+
+export async function deletePersonen(page: Page, personId: string): Promise<void> {
+    const response = await page.request.delete(FRONTEND_URL + `api/personen/${personId}`, {});
+}
+
+export async function deleteRolle(page: Page, RolleId: string): Promise<void> {
+    const response = await page.request.delete(FRONTEND_URL + `api/rollen/${RolleId}`, {});
+}
---
+++ tests/SchulportalAdministration.spec.ts
@@ -0,0 +1,81 @@
+import {expect, Page, test} from "@playwright/test";
+import {LandingPage} from "../pages/LandingView.page";
+import {StartPage} from "../pages/StartView.page";
+import {LoginPage} from "../pages/LoginView.page";
+import {HeaderPage} from "../pages/Header.page";
+import {createBenutzerWithUserContext, UserInfo, getSPId, addSystemrechtToRolle, deletePersonen, deleteRolle } from "../base/testHelper";
+
+const PW = process.env.PW;
+const ADMIN = process.env.USER;
+const FRONTEND_URL = process.env.FRONTEND_URL || "";
+
+test.describe(`Testfälle für Schulportal Administration": Umgebung: ${process.env.UMGEBUNG}: URL: ${process.env.FRONTEND_URL}:`, () => {
+    test("Checken, ob die Schulportal-Administration Kachel nicht sichtbar ist für Lehrkräfte", async ({page}) => {
+        const Landing = new LandingPage(page);
+        const Login = new LoginPage(page);
+
+        // Testdaten erstellen
+        const idSP = await getSPId(page, 'E-Mail');
+        const userInfo: UserInfo = await createBenutzerWithUserContext(page, 'Testschule Schulportal', 'LEHR', 'MeierLehrer', 'Hans', idSP, 'RolleA');
+        const Header = new HeaderPage(page);
+
+        // Test durchführen
+        await Landing.button_Anmelden.click();
+        await Login.login(userInfo.username, userInfo.password);
+        await Login.UpdatePW();
+        const Startseite = new StartPage(page);
+        await test.step(`Prüfen, dass die Kachel E-Mail angezeigt wird und die Kachel Schulportal-Administration nicht angezeigt wird`, async () => {
+            await expect(Startseite.card_item_schulportal_administration).toBeHidden();
+            await expect(Startseite.card_item_email).toBeVisible();
+        });
+        await Header.button_logout.click();
+
+        // Testdaten löschen
+        await Header.button_logout.click();
+        await Landing.button_Anmelden.click();
+        await Login.login(ADMIN, PW);
+        await deletePersonen(page, userInfo.personId);
+        await deleteRolle(page, userInfo.rolleId);
+         
+        await page.pause();
+    });
+
+    test.only("Checken, ob die Schulportal-Administration Kachel sichtbar ist für Schuladmins", async ({page}) => {
+        const Landing = new LandingPage(page);
+        const Login = new LoginPage(page);
+        
+        // Testdaten erstellen
+        const idSP = await getSPId(page, 'Schulportal-Administration');
+        const userInfo: UserInfo = await createBenutzerWithUserContext(page, 'Testschule Schulportal', 'LEIT', 'MeierSchulAdmin', 'Peter', idSP, 'RolleB');
+        const Header = new HeaderPage(page);
+
+        await page.goto(FRONTEND_URL);          
+        await Landing.button_Anmelden.click();
+        await Login.login(ADMIN, PW);
+        await addSystemrechtToRolle(page, userInfo.rolleId, 'PERSONEN_VERWALTEN'); 
+        await Header.button_logout.click();     
+
+        // Test durchführen
+        await Landing.button_Anmelden.click();
+        await Login.login(userInfo.username, userInfo.password);
+        await Login.UpdatePW();
+        const Startseite = new StartPage(page);
+        await test.step(`Prüfen, dass die Kachel E-Mail angezeigt wird und die Kachel Schulportal-Administration nicht angezeigt wird`, async () => {
+            await expect(Startseite.card_item_schulportal_administration).toBeVisible();
+            await expect(Startseite.card_item_email).toBeHidden();
+        });
+        await Header.button_logout.click();
+
+        // Testdaten löschen
+        await Header.button_logout.click();
+        await Landing.button_Anmelden.click();
+        await Login.login(ADMIN, PW);
+        await deletePersonen(page, userInfo.personId);
+        await deleteRolle(page, userInfo.rolleId);
+         
+        await page.pause();
+    }); 
+});
+
+       
+
