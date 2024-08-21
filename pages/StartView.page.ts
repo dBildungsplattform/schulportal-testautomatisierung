fa865f765e65381a1530ed3e8f42ac3a96b65ffa
Marvin Rode <marvin.rode@capgemini.com>
2024-08-21 14:35:25 +0200

    Fix tests and formatting

--- pages/StartView.page.ts
+++ pages/StartView.page.ts
@@ -11,9 +11,7 @@ export class StartPage{
         this.page = page;  
         this.text_h2_Ueberschrift = page.getByTestId('all-service-provider-title');
         this.card_item_email = page.locator('[href="https://de.wikipedia.org/wiki/E-Mail"]').first();
-        this.card_item_itslearning = page.locator(
-          '[href="https://sh-staging.itslintegrations.com/"]'
-        );
+        this.card_item_itslearning = page.locator('[href="https://sh-staging.itslintegrations.com/"]');
         this.card_item_schulportal_administration = page.getByText('Schulportal-Administration');
     }
 }
