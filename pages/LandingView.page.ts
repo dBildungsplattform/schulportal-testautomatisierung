80041bd966231865b9973c6727a89e89dfbf2618
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2024-08-28 11:26:45 +0200

    Added Navigation paths

--- pages/LandingView.page.ts
+++ pages/LandingView.page.ts
@@ -1,13 +1,25 @@
-import { type Locator, Page } from '@playwright/test';
+import {expect, type Locator, Page} from '@playwright/test';
 
 export class LandingPage{
     readonly page: Page;
+    readonly url: string;
     readonly text_Willkommen: Locator;
     readonly button_Anmelden: Locator;
 
-    constructor(page){
-        this.page = page;  
+    constructor(page, url){
+        this.page = page;
+        this.url = url;
         this.text_Willkommen = page.getByTestId('landing-headline');
         this.button_Anmelden = page.getByTestId('login-button');
+    }
+
+    async goto() {
+        await this.page.goto(this.url);
+        await expect(this.text_Willkommen).toBeVisible();
+    }
+
+    async login() {
+        this.goto();
+        this.button_Anmelden.click();
     }
 }
