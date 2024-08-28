ac3cb7af744c92fafb676a45dbc1e9bbc24e7529
Kristoff Kiefer <kristoff.kiefer@capgemini.com>
2024-08-28 11:52:38 +0200

    Frontend URL wird per Default übergeben

--- pages/LandingView.page.ts
+++ pages/LandingView.page.ts
@@ -1,12 +1,14 @@
 import {expect, type Locator, Page} from '@playwright/test';
 
+const FRONTEND_URL = process.env.FRONTEND_URL || "";
+
 export class LandingPage{
     readonly page: Page;
     readonly url: string;
     readonly text_Willkommen: Locator;
     readonly button_Anmelden: Locator;
 
-    constructor(page, url){
+    constructor(page, url = FRONTEND_URL){
         this.page = page;
         this.url = url;
         this.text_Willkommen = page.getByTestId('landing-headline');
