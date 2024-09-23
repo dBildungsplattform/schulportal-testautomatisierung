77635835a07f9932161244d66d6ea1b48a2d58d6
jakobmadp~ <manfred.jakob@dataport.de>
2024-09-23 13:19:52 +0200

    Schreibweise KlasseCreationView.page.ts angepasst

--- pages/admin/KlasseCreationView.page.ts
+++ pages/admin/KlasseCreationView.page.ts
@@ -2,38 +2,38 @@ import { type Locator, Page } from '@playwright/test';
 
 export class KlasseCreationViewPage{
     readonly page: Page;
-    readonly text_h2_KlasseAnlegen: Locator;
-    readonly button_Schliessen: Locator;
-    readonly combobox_Schulstrukturknoten: Locator;
-    readonly input_Klassenname: Locator;
-    readonly button_KlasseAnlegen: Locator;
+    readonly text_h2_klasseAnlegen: Locator;
+    readonly button_schliessen: Locator;
+    readonly combobox_schulstrukturknoten: Locator;
+    readonly input_klassenname: Locator;
+    readonly button_klasseAnlegen: Locator;
     readonly text_success: Locator;
     readonly icon_success: Locator;
-    readonly text_DatenGespeichert: Locator;
-    readonly label_Schule: Locator;
-    readonly data_Schule: Locator;
-    readonly label_Klasse: Locator;
-    readonly data_Klasse: Locator;
-    readonly button_ZurueckErgebnisliste: Locator;
-    readonly button_WeitereKlasseAnlegen: Locator;
+    readonly text_datenGespeichert: Locator;
+    readonly label_schule: Locator;
+    readonly data_schule: Locator;
+    readonly label_klasse: Locator;
+    readonly data_klasse: Locator;
+    readonly button_zurueckErgebnisliste: Locator;
+    readonly button_weitereKlasseAnlegen: Locator;
    
     constructor(page){
         // Anlage Klasse
         this.page = page;
-        this.text_h2_KlasseAnlegen = page.getByTestId('layout-card-headline');
-        this.button_Schliessen = page.getByTestId('close-layout-card-button');
-        this.combobox_Schulstrukturknoten = page.getByTestId('schule-select').locator('.v-input__control'); 
-        this.input_Klassenname = page.getByTestId('klassenname-input').locator('input');
-        this.button_KlasseAnlegen = page.getByTestId('klasse-form-create-button');
+        this.text_h2_klasseAnlegen = page.getByTestId('layout-card-headline');
+        this.button_schliessen = page.getByTestId('close-layout-card-button');
+        this.combobox_schulstrukturknoten = page.getByTestId('schule-select').locator('.v-input__control'); 
+        this.input_klassenname = page.getByTestId('klassenname-input').locator('input');
+        this.button_klasseAnlegen = page.getByTestId('klasse-form-create-button');
         // Bestätigungsseite Klasse
         this.text_success = page.getByTestId('klasse-success-text');
         this.icon_success = page.locator('.mdi-check-circle');
-        this.text_DatenGespeichert = page.getByText('Folgende Daten wurden gespeichert:');
-        this.label_Schule =  page.getByText('Schule:', { exact: true });
-        this.data_Schule =  page.getByTestId('created-klasse-schule');
-        this.label_Klasse =  page.getByText('Klassenname:', { exact: true });
-        this.data_Klasse =  page.getByTestId('created-klasse-name');
-        this.button_ZurueckErgebnisliste = page.getByTestId('back-to-list-button');
-        this.button_WeitereKlasseAnlegen = page.getByTestId('create-another-klasse-button');
+        this.text_datenGespeichert = page.getByText('Folgende Daten wurden gespeichert:');
+        this.label_schule =  page.getByText('Schule:', { exact: true });
+        this.data_schule =  page.getByTestId('created-klasse-schule');
+        this.label_klasse =  page.getByText('Klassenname:', { exact: true });
+        this.data_klasse =  page.getByTestId('created-klasse-name');
+        this.button_zurueckErgebnisliste = page.getByTestId('back-to-list-button');
+        this.button_weitereKlasseAnlegen = page.getByTestId('create-another-klasse-button');
     }
 }
