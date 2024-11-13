6e40f398fbf31641f85911a8e4cd0eb742ebaec9
jakobmadp~ <manfred.jakob@dataport.de>
2024-11-13 15:29:10 +0100

    merged main into branch

--- tests/PersonBearbeiten.spec.ts
+++ tests/PersonBearbeiten.spec.ts
@@ -17,11 +17,9 @@ import {testschule} from "../base/organisation.ts";
 import {email} from "../base/sp.ts";
 import {generateLehrerNachname, generateLehrerVorname, generateRolleName} from "../base/testHelperGenerateTestdataNames.ts";
 import {generateDateFuture, generateDateToday, gotoTargetURL} from "../base/testHelperUtils.ts";
-import FromAnywhere from "../pages/FromAnywhere";
 
 const PW = process.env.PW;
 const ADMIN = process.env.USER;
-//const FRONTEND_URL = process.env.FRONTEND_URL || "";
 
 let username: string[] = []; // Im afterEach Block werden alle Testdaten gelöscht
 let rolleId: string[] = []; // Im afterEach Block werden alle Testdaten gelöscht
@@ -73,7 +71,7 @@ test.describe(`Testfälle für die Administration von Personen": Umgebung: ${pro
         });
     });
 
-  test("Eine Schulzuordnung bei einem bestehenden Benutzer hinzufügen", {tag: [LONG]}, async ({ page }) => {
+  test("Eine Schulzuordnung bei einem bestehenden Benutzer hinzufügen", {tag: [LONG, STAGE]}, async ({ page }) => {
     const personManagementView = new PersonManagementViewPage(page);
     const PersonDetailsView = new PersonDetailsViewPage(page);
     const header = new HeaderPage(page);
@@ -81,72 +79,72 @@ test.describe(`Testfälle für die Administration von Personen": Umgebung: ${pro
     const login = new LoginPage(page);
     const startseite = new StartPage(page);
 
-        const addminVorname = "TAuto-PW-V-" + faker.person.firstName();
-        const adminNachname = "TAuto-PW-N-" + faker.person.lastName();
-        const adminRolle = "TAuto-PW-LEIT-" + faker.lorem.word({length: {min: 8, max: 12}});
-        const adminRollenart = 'LEIT';
-        const adminOrganisation = 'Testschule-PW665';
-        const adminIdSP = await getSPId(page, 'Schulportal-Administration');
-        let userInfoAdmin: UserInfo;
+    const addminVorname = "TAuto-PW-V-" + faker.person.firstName();
+    const adminNachname = "TAuto-PW-N-" + faker.person.lastName();
+    const adminRolle = "TAuto-PW-LEIT-" + faker.lorem.word({length: {min: 8, max: 12}});
+    const adminRollenart = 'LEIT';
+    const adminOrganisation = 'Testschule-PW665';
+    const adminIdSP = await getSPId(page, 'Schulportal-Administration');
+    let userInfoAdmin: UserInfo;
 
-        const lehrerVorname = "TAuto-PW-V-" + faker.person.firstName();
-        const lehrerNachname = "TAuto-PW-N-" + faker.person.lastName();
-        const lehrerRolle = "TAuto-PW-LEHR-" + faker.lorem.word({length: {min: 8, max: 12}});
-        const lehrerRollenart = 'LEHR';
-        const lehrerOrganisation = 'Testschule-PW665';
-        const lehrerIdSP = await getSPId(page, 'E-Mail');
-        let userInfoLehrer: UserInfo;
-        let lehrerBenutzername = '';
-        const rolle = 'LiV';
-        const kopersNr = faker.number.bigInt({min: 100000, max: 900000}).toString();
+    const lehrerVorname = "TAuto-PW-V-" + faker.person.firstName();
+    const lehrerNachname = "TAuto-PW-N-" + faker.person.lastName();
+    const lehrerRolle = "TAuto-PW-LEHR-" + faker.lorem.word({length: {min: 8, max: 12}});
+    const lehrerRollenart = 'LEHR';
+    const lehrerOrganisation = 'Testschule-PW665';
+    const lehrerIdSP = await getSPId(page, 'E-Mail');
+    let userInfoLehrer: UserInfo;
+    let lehrerBenutzername = '';
+    const rolle = 'LiV';
+    const kopersNr = faker.number.bigInt({min: 100000, max: 900000}).toString();
 
-        await test.step(`Einen Schuladmin und einen zu bearbeitenden Lehrer mit je einer einer Schulzuordnung(Schule ist an einer Position > 25 in der DB) über die api anlegen und mit diesem Schuladmin anmelden`, async () => {
-            // Schuladmin
-            userInfoAdmin = await createRolleAndPersonWithUserContext(page, adminOrganisation, adminRollenart, addminVorname, adminNachname, adminIdSP, adminRolle);
-            await addSystemrechtToRolle(page, userInfoAdmin.rolleId, 'PERSONEN_VERWALTEN');
-            username.push(userInfoAdmin.username);
-            rolleId.push(userInfoAdmin.rolleId);
+    await test.step(`Einen Schuladmin und einen zu bearbeitenden Lehrer mit je einer einer Schulzuordnung(Schule ist an einer Position > 25 in der DB) über die api anlegen und mit diesem Schuladmin anmelden`, async () => {
+        // Schuladmin
+        userInfoAdmin = await createRolleAndPersonWithUserContext(page, adminOrganisation, adminRollenart, addminVorname, adminNachname, adminIdSP, adminRolle);
+        await addSystemrechtToRolle(page, userInfoAdmin.rolleId, 'PERSONEN_VERWALTEN');
+        username.push(userInfoAdmin.username);
+        rolleId.push(userInfoAdmin.rolleId);
 
-            // Lehrer
-            userInfoLehrer = await createRolleAndPersonWithUserContext(page, lehrerOrganisation, lehrerRollenart, lehrerVorname, lehrerNachname, lehrerIdSP, lehrerRolle);
-            username.push(userInfoLehrer.username);
-            rolleId.push(userInfoLehrer.rolleId);
-            lehrerBenutzername = userInfoLehrer.username;
+        // Lehrer
+        userInfoLehrer = await createRolleAndPersonWithUserContext(page, lehrerOrganisation, lehrerRollenart, lehrerVorname, lehrerNachname, lehrerIdSP, lehrerRolle);
+        username.push(userInfoLehrer.username);
+        rolleId.push(userInfoLehrer.rolleId);
+        lehrerBenutzername = userInfoLehrer.username;
 
-            await header.logout();
-            await landing.button_Anmelden.click();
-            await login.login(userInfoAdmin.username, userInfoAdmin.password);
-            await login.UpdatePW();
-            await expect(startseite.text_h2_Ueberschrift).toBeVisible();
-        })
+        await header.logout();
+        await landing.button_Anmelden.click();
+        await login.login(userInfoAdmin.username, userInfoAdmin.password);
+        await login.UpdatePW();
+        await expect(startseite.text_h2_Ueberschrift).toBeVisible();
+    })
 
-        await test.step(`Die Gesamtübersicht des Lehrers öffnen`, async () => {
-            await gotoTargetURL(page, "admin/personen");
-            await personManagementView.input_Suchfeld.fill(lehrerBenutzername);
-            await personManagementView.button_Suchen.click();
-            await page.getByRole("cell", {name: lehrerBenutzername, exact: true}).click();
-        });
+    await test.step(`Die Gesamtübersicht des Lehrers öffnen`, async () => {
+        await gotoTargetURL(page, "admin/personen");
+        await personManagementView.input_Suchfeld.fill(lehrerBenutzername);
+        await personManagementView.button_Suchen.click();
+        await page.getByRole("cell", {name: lehrerBenutzername, exact: true}).click();
+    });
 
-        await test.step(`Eine zweite Schulzuordnung hinzufügen`, async () => {
-            await PersonDetailsView.button_editSchulzuordnung.click();
-            await PersonDetailsView.button_addSchulzuordnung.click();
-            expect(await PersonDetailsView.combobox_organisation.innerText()).toContain(adminOrganisation);
-            await PersonDetailsView.combobox_rolle.click();
-            await page.getByText(rolle, {exact: true}).click();
-            await PersonDetailsView.input_kopersNr.fill(kopersNr);
-            await PersonDetailsView.button_submitAddSchulzuordnung.click();
-            await PersonDetailsView.button_confirmAddSchulzuordnung.click();
-            await PersonDetailsView.button_saveAssignmentChanges.click();
-            await PersonDetailsView.button_closeSaveAssignmentChanges.click();
-        });
+    await test.step(`Eine zweite Schulzuordnung hinzufügen`, async () => {
+        await PersonDetailsView.button_editSchulzuordnung.click();
+        await PersonDetailsView.button_addSchulzuordnung.click();
+        expect(await PersonDetailsView.combobox_organisation.innerText()).toContain(adminOrganisation);
+        await PersonDetailsView.combobox_rolle.click();
+        await page.getByText(rolle, {exact: true}).click();
+        await PersonDetailsView.input_kopersNr.fill(kopersNr);
+        await PersonDetailsView.button_submitAddSchulzuordnung.click();
+        await PersonDetailsView.button_confirmAddSchulzuordnung.click();
+        await PersonDetailsView.button_saveAssignmentChanges.click();
+        await PersonDetailsView.button_closeSaveAssignmentChanges.click();
+    });
 
-        await test.step(`In der Gesamtübersicht die neue Schulzuordnung prüfen`, async () => {
-            await expect(page.getByTestId('person-details-card')).toContainText('1111165 (Testschule-PW665): LiV (befristet bis');
-            await expect(page.getByTestId('person-details-card')).toContainText('1111165 (Testschule-PW665): ' + lehrerRolle);
+    await test.step(`In der Gesamtübersicht die neue Schulzuordnung prüfen`, async () => {
+        await expect(page.getByTestId('person-details-card')).toContainText('1111165 (Testschule-PW665): LiV (befristet bis');
+        await expect(page.getByTestId('person-details-card')).toContainText('1111165 (Testschule-PW665): ' + lehrerRolle);
         });
     })
 
-    test("Einen Benutzer über das FE unbefristet sperren @long @stage", async ({page,}) => {
+    test("Einen Benutzer über das FE unbefristet sperren @long @stage", {tag: [LONG, STAGE]}, async ({ page }) => {
         let userInfoLehrer: UserInfo;
         const sperrDatumAb = await generateDateToday() // Konkrete Testdaten für diesen Testfall
 
@@ -171,7 +169,7 @@ test.describe(`Testfälle für die Administration von Personen": Umgebung: ${pro
         })
     })
 
-    test("Einen Benutzer über das FE befristet sperren @long @stage", async ({page,}) => {
+    test("Einen Benutzer über das FE befristet sperren @long @stage", {tag: [LONG, STAGE]}, async ({ page }) => {
         let userInfoLehrer: UserInfo;
         const sperrDatumAb = await generateDateToday() // Konkrete Testdaten für diesen Testfall
         const sperrDatumBis = await generateDateFuture(5, 2); // Konkrete Testdaten für diesen Testfall
