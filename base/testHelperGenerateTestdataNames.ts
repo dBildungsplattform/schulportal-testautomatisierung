2c3a27420342c59c2cf4f7bb83325f97c9a0b6b4
jakobmadp~ <manfred.jakob@dataport.de>
2024-11-22 14:17:52 +0100

    minor changes

--- base/testHelperGenerateTestdataNames.ts
+++ base/testHelperGenerateTestdataNames.ts
@@ -13,6 +13,6 @@ export async function generateRolleName(){
   return "TAuto-PW-R-" + faker.lorem.word({ length: { min: 7, max: 7 }}) + generateRandomString({length: 3,charSetType: CharacterSetType.Alphabetic});
 }
 
-export async function generateKopersMr(){  
+export async function generateKopersNr(){  
   return '0815' + faker.string.numeric({ length: 3 });
 }
