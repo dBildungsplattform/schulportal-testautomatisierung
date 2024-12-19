0531d41a0a5c020420bba1b735ed45cdd6212f21
jakobmadp~ <manfred.jakob@dataport.de>
2024-12-19 17:24:07 +0100

    fixed test

--- base/rollen.ts
+++ base/rollen.ts
@@ -2,3 +2,4 @@ export const schuelerRolle = "itslearning-Schüler";
 export const landesadminRolle = "Landesadministrator";
 export const schuladminOeffentlichRolle = "Schuladministrator öffentlich";
 export const lehrkraftOeffentlichRolle = "Lehrkraft";
+export const lehrkraftInVertretungRolle = "LiV";
