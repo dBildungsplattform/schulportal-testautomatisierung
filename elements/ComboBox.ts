b6d9a47a0b8a8f8721066adea1ef72684359926e
Yannick Clausen <yannick.clausen@capgemini.com>
2025-01-06 14:41:39 +0100

    fix flakyness with combobox loading time

--- elements/ComboBox.ts
+++ elements/ComboBox.ts
@@ -1,17 +1,25 @@
-import { type Locator, Page } from '@playwright/test';
+import { expect, type Locator, Page } from '@playwright/test';
 
+const noDataMessage: string = 'Keine Daten gefunden.';
 export class ComboBox {
+  private readonly overlayLocator: Locator;
   private readonly itemsLocator: Locator;
   private readonly modalToggle: Locator;
 
   constructor(private readonly page: Page, private readonly locator: Locator) {
+    this.overlayLocator = this.page.locator('div.v-overlay.v-menu');
     this.itemsLocator = this.page.locator('div.v-overlay.v-menu div.v-list-item');
     this.modalToggle = this.locator.locator('.v-field__append-inner');
+  }
+
+  private async waitForData(): Promise<void> {
+    await expect(this.overlayLocator).not.toHaveText(noDataMessage);
   }
 
   public async selectByPosition(selection: number[]): Promise<string[]> {
     const selectedItems: string[] = [];
     await this.openModal();
+    await this.waitForData();
     const items: Locator = this.itemsLocator;
     for (const index of selection) {
       const item: Locator = items.nth(index);
@@ -24,6 +32,7 @@ export class ComboBox {
 
   public async selectByTitle(title: string): Promise<void> {
     await this.openModal();
+    await this.waitForData();
     const item: Locator = this.itemsLocator.filter({
       has: this.page.getByText(title, { exact: true }),
     });
@@ -51,7 +60,7 @@ export class ComboBox {
     const item: Locator = this.itemsLocator.filter({
       has: this.page.getByText(title, { exact: true }),
     });
-    await item.waitFor({ state: "visible" });
+    await item.waitFor({ state: 'visible' });
     await item.click();
   }
 }
