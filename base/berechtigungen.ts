35bcf0eaa368babf524d85967a0c0750b7b1c64d
Manfred <141728305+jakobmadp@users.noreply.github.com>
2025-02-20 18:13:21 +0100

    Update base/berechtigungen.ts
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>

--- base/berechtigungen.ts
+++ base/berechtigungen.ts
@@ -2,6 +2,6 @@ export const rollenVerwalten: string = "ROLLEN_VERWALTEN";
 export const personenSofortLoeschen: string = "PERSONEN_SOFORT_LOESCHEN";
 export const personenVerwalten: string = "PERSONEN_VERWALTEN";
 export const schulenVerwalten: string = "SCHULEN_VERWALTEN";
-export const KlassenVerwalten: string = "KLASSEN_VERWALTEN";
+export const klassenVerwalten: string = "KLASSEN_VERWALTEN";
 export const schultraegerVerwalten: string = "SCHULTRAEGER_VERWALTEN";
 export const personenAnlegen: string = "PERSONEN_ANLEGEN";
