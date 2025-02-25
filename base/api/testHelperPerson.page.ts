9e3c196177ed4a5cd42b4ce05bf5e11b29ac55f7
Manfred <141728305+jakobmadp@users.noreply.github.com>
2025-02-25 13:34:38 +0100

    Spsh 966 set pw via profil (#141)
    * new test case pw reset from profile view as teacher
    
    * new test case pw reset from profile view as student
    
    * refactoring
    
    * Update base/berechtigungen.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update pages/Header.page.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update pages/LoginView.page.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * fix findings from review
    
    * fix findings from review
    
    * fix findings from review
    
    * Update tests/Person.spec.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * Update tests/PersonBearbeiten.spec.ts
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>
    
    * fix findings from review
    
    ---------
    
    Co-authored-by: Julian Aggarwal <141040785+jaggarnaut@users.noreply.github.com>

--- base/api/testHelperPerson.page.ts
+++ base/api/testHelperPerson.page.ts
@@ -1,172 +1,195 @@
 import { Page, expect } from '@playwright/test';
-import { getOrganisationId } from "./testHelperOrganisation.page";
-import { createRolle, addSPToRolle, getRolleId } from "./testHelperRolle.page";
-import { getSPId } from "./testHelperServiceprovider.page";
-import { UserInfo } from "./testHelper.page";
+import { getOrganisationId } from './testHelperOrganisation.page';
+import { createRolle, addSPToRolle, getRolleId } from './testHelperRolle.page';
+import { getSPId } from './testHelperServiceprovider.page';
+import { UserInfo } from './testHelper.page';
 import { HeaderPage } from '../../pages/Header.page';
 import { LoginPage } from '../../pages/LoginView.page';
-import { generateNachname, generateVorname, generateKopersNr, generateRolleName } from "../testHelperGenerateTestdataNames";
-import { testschule } from "../organisation";
-import { email, kalender, adressbuch } from "../sp";
-import { typeLehrer } from "../rollentypen";
+import {
+  generateNachname,
+  generateVorname,
+  generateKopersNr,
+  generateRolleName,
+} from '../testHelperGenerateTestdataNames';
+import { testschule } from '../organisation';
+import { email, kalender, adressbuch } from '../sp';
+import { typeLehrer } from '../rollentypen';
 
-const FRONTEND_URL: string | undefined = process.env.FRONTEND_URL || "";
+const FRONTEND_URL: string | undefined = process.env.FRONTEND_URL || '';
 
 export async function createPerson(
-    page: Page,
-    familienname: string,
-    vorname: string,
-    organisationId: string,
-    rolleId: string,
-    koPersNr?: string,
-    klasseId?: string
+  page: Page,
+  familienname: string,
+  vorname: string,
+  organisationId: string,
+  rolleId: string,
+  koPersNr?: string,
+  klasseId?: string
 ): Promise<UserInfo> {
-    let requestData: any;
-    
-    requestData = {
-        data: {
-            familienname,
-            vorname,
-            createPersonenkontexte: [
-                {
-                    "organisationId": organisationId,
-                    "rolleId": rolleId
-                }
-            ]
-        },
-        failOnStatusCode: false, 
-        maxRetries: 3
-    }; 
+  let requestData: any;
 
-    if(klasseId) {
-        requestData.data.createPersonenkontexte.push(
-            {
-                "organisationId": klasseId,
-                "rolleId": rolleId
-            }
-        )
-    }
-    
-    if(koPersNr) {
-        requestData.data['personalnummer'] = koPersNr;
-    }
-    
-    const response = await page.request.post(FRONTEND_URL + 'api/personenkontext-workflow/', requestData);
-    expect(response.status()).toBe(201);
-    const json = await response.json();
-    return {
-        username: json.person.referrer,
-        password: json.person.startpasswort,
-        rolleId: rolleId,
-        organisationId: organisationId,
-        personId: json.person.id
-    }
+  requestData = {
+    data: {
+      familienname,
+      vorname,
+      createPersonenkontexte: [
+        {
+          organisationId: organisationId,
+          rolleId: rolleId,
+        },
+      ],
+    },
+    failOnStatusCode: false,
+    maxRetries: 3,
+  };
+
+  if (klasseId) {
+    requestData.data.createPersonenkontexte.push({
+      organisationId: klasseId,
+      rolleId: rolleId,
+    });
+  }
+
+  if (koPersNr) {
+    requestData.data['personalnummer'] = koPersNr;
+  }
+
+  const response = await page.request.post(FRONTEND_URL + 'api/personenkontext-workflow/', requestData);
+  expect(response.status()).toBe(201);
+  const json = await response.json();
+  return {
+    username: json.person.referrer,
+    password: json.person.startpasswort,
+    rolleId: rolleId,
+    organisationId: organisationId,
+    personId: json.person.id,
+  };
 }
 
 export async function createPersonWithUserContext(
-    page: Page,
-    organisationName: string,
-    familienname: string,
-    vorname: string,
-    rolleName: string,
-    koPersNr?: string
+  page: Page,
+  organisationName: string,
+  familienname: string,
+  vorname: string,
+  rolleName: string,
+  koPersNr?: string
 ): Promise<UserInfo> {
-    // Organisation wird nicht angelegt, da diese zur Zeit nicht gelöscht werden kann
-    // API-Calls machen und Benutzer mit Kontext anlegen
-    const organisationId: string = await getOrganisationId(page, organisationName);
-    const rolleId: string = await getRolleId(page, rolleName);
-    const userInfo: UserInfo = await createPerson(page, familienname, vorname, organisationId, rolleId, koPersNr);
-    return userInfo;
+  // Organisation wird nicht angelegt, da diese zur Zeit nicht gelöscht werden kann
+  // API-Calls machen und Benutzer mit Kontext anlegen
+  const organisationId: string = await getOrganisationId(page, organisationName);
+  const rolleId: string = await getRolleId(page, rolleName);
+  const userInfo: UserInfo = await createPerson(page, familienname, vorname, organisationId, rolleId, koPersNr);
+  return userInfo;
 }
 
 export async function createRolleAndPersonWithUserContext(
-    page: Page,
-    organisationName: string,
-    rollenArt: string,
-    familienname: string,
-    vorname: string,
-    idSPs: string[],
-    rolleName: string,
-    koPersNr?: string,
-    klasseId?: string,
+  page: Page,
+  organisationName: string,
+  rollenArt: string,
+  familienname: string,
+  vorname: string,
+  idSPs: string[],
+  rolleName: string,
+  koPersNr?: string,
+  klasseId?: string
 ): Promise<UserInfo> {
-    // Organisation wird nicht angelegt, da diese zur Zeit nicht gelöscht werden kann
-    // API-Calls machen und Benutzer mit Kontext anlegen
-    const organisationId: string = await getOrganisationId(page, organisationName);
-    const rolleId: string = await createRolle(page, rollenArt, organisationId, rolleName);
+  // Organisation wird nicht angelegt, da diese zur Zeit nicht gelöscht werden kann
+  // API-Calls machen und Benutzer mit Kontext anlegen
+  const organisationId: string = await getOrganisationId(page, organisationName);
+  const rolleId: string = await createRolle(page, rollenArt, organisationId, rolleName);
 
-    await addSPToRolle(page, rolleId, idSPs);
-    const userInfo: UserInfo = await createPerson(
-        page,
-        familienname,
-        vorname,
-        organisationId,
-        rolleId,
-        koPersNr,
-        klasseId,
-    );
-    return userInfo;
+  await addSPToRolle(page, rolleId, idSPs);
+  const userInfo: UserInfo = await createPerson(
+    page,
+    familienname,
+    vorname,
+    organisationId,
+    rolleId,
+    koPersNr,
+    klasseId
+  );
+  return userInfo;
 }
 
-export async function addSecondOrganisationToPerson(page: Page, personId: string, organisationId1: string, organisationId2: string, rolleId: string) {
-    const response = await page.request.put(FRONTEND_URL + 'api/personenkontext-workflow/' + personId, {
-        data: {
-            "lastModified":"2034-09-11T08:28:36.590Z",
-            "count": 1,
-            "personenkontexte":
-            [
-                {
-                    "personId": personId,
-                    "organisationId": organisationId1,
-                    "rolleId": rolleId
-                },
-                {
-                    "personId": personId,
-                    "organisationId": organisationId2,
-                    "rolleId": rolleId
-                }
-            ]
+export async function addSecondOrganisationToPerson(
+  page: Page,
+  personId: string,
+  organisationId1: string,
+  organisationId2: string,
+  rolleId: string
+) {
+  const response = await page.request.put(FRONTEND_URL + 'api/personenkontext-workflow/' + personId, {
+    data: {
+      lastModified: '2034-09-11T08:28:36.590Z',
+      count: 1,
+      personenkontexte: [
+        {
+          personId: personId,
+          organisationId: organisationId1,
+          rolleId: rolleId,
         },
-        failOnStatusCode: false, 
-        maxRetries: 3
-    });
-    expect(response.status()).toBe(200);
+        {
+          personId: personId,
+          organisationId: organisationId2,
+          rolleId: rolleId,
+        },
+      ],
+    },
+    failOnStatusCode: false,
+    maxRetries: 3,
+  });
+  expect(response.status()).toBe(200);
 }
 
 export async function deletePerson(page: Page, personId: string): Promise<void> {
-    const response = await page.request.delete(FRONTEND_URL + `api/personen/${personId}`, {failOnStatusCode: false, maxRetries: 3});
-    expect(response.status()).toBe(204);
+  const response = await page.request.delete(FRONTEND_URL + `api/personen/${personId}`, {
+    failOnStatusCode: false,
+    maxRetries: 3,
+  });
+  expect(response.status()).toBe(204);
 }
 
 export async function getPersonId(page: Page, searchString: string): Promise<string> {
-    const response = await page.request.get(FRONTEND_URL + `api/personen-frontend?suchFilter=${searchString}`, {failOnStatusCode: false, maxRetries: 3});
-    expect(response.status()).toBe(200);
-    const json = await response.json();
-    return json.items[0].person.id;
+  const response = await page.request.get(FRONTEND_URL + `api/personen-frontend?suchFilter=${searchString}`, {
+    failOnStatusCode: false,
+    maxRetries: 3,
+  });
+  expect(response.status()).toBe(200);
+  const json = await response.json();
+  return json.items[0].person.id;
 }
 
 export async function createTeacherAndLogin(page: Page) {
-    const header = new HeaderPage(page);
-    const login: LoginPage = new LoginPage(page);
-    const userInfo: UserInfo = await createRolleAndPersonWithUserContext(page, testschule, typeLehrer, await generateNachname(), await generateVorname(), [await getSPId(page, email), await getSPId(page, kalender), await getSPId(page, adressbuch)], await generateRolleName(), await generateKopersNr());
-   
-    await header.logout();
-    await header.button_login.click();
-    await login.login(userInfo.username, userInfo.password);
-    await login.UpdatePW();
-    await expect(header.icon_myProfil).toBeVisible(); 
-    await expect(header.icon_logout).toBeVisible();
-    return userInfo;
+  const header: HeaderPage = new HeaderPage(page);
+  const login: LoginPage = new LoginPage(page);
+  const userInfo: UserInfo = await createRolleAndPersonWithUserContext(
+    page,
+    testschule,
+    typeLehrer,
+    await generateNachname(),
+    await generateVorname(),
+    [await getSPId(page, email), await getSPId(page, kalender), await getSPId(page, adressbuch)],
+    await generateRolleName(),
+    await generateKopersNr()
+  );
+
+  await header.logout();
+  await header.button_login.click();
+  await login.login(userInfo.username, userInfo.password);
+  await login.updatePW();
+  await expect(header.icon_myProfil).toBeVisible();
+  await expect(header.icon_logout).toBeVisible();
+  return userInfo;
 }
 
 export async function lockPerson(page: Page, personId: string, organisationId: string): Promise<void> {
-    const response = await page.request.put(FRONTEND_URL + `api/personen/${personId}/lock-user`, {
-        data: {
-            "lock": true,
-            "locked_by": organisationId
-        },
-        failOnStatusCode: false, 
-        maxRetries: 3
-    });
-    expect(response.status()).toBe(202);
+  const response = await page.request.put(FRONTEND_URL + `api/personen/${personId}/lock-user`, {
+    data: {
+      lock: true,
+      locked_by: organisationId,
+    },
+    failOnStatusCode: false,
+    maxRetries: 3,
+  });
+  expect(response.status()).toBe(202);
 }
