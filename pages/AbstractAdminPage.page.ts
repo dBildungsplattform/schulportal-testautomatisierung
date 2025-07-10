28e3ea5fc424346692f8d9ca5347b7efcedd5ee0
Yannick Clausen <yannick.clausen@capgemini.com>
2025-07-10 10:55:38 +0200

    add abstract base for pages

---
+++ pages/AbstractAdminPage.page.ts
@@ -0,0 +1,19 @@
+import { Page } from '@playwright/test';
+import { HeaderPage } from './components/Header.page';
+import { MenuPage } from './components/MenuBar.page';
+
+/**
+ * Base class for all pages in the Schulportal after login.
+ */
+export abstract class AbstractAdminPage {
+  readonly menu: MenuPage;
+  readonly header: HeaderPage;
+
+  constructor(protected readonly page: Page) {
+    this.menu = new MenuPage(page);
+    this.header = new HeaderPage(page);
+  }
+
+  abstract waitForPageLoad(): Promise<void>;
+
+}
