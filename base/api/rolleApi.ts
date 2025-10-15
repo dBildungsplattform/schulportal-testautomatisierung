a2accbeea4bd6a0c80d34786c7ea43affa4529ac
jaggarnaut <julian.aggarwal@dataport.de>
2025-10-15 15:11:09 +0200

    fixed status code

--- base/api/rolleApi.ts
+++ base/api/rolleApi.ts
@@ -64,7 +64,7 @@ export async function addServiceProvidersToRolle(page: Page, rolleId: string, se
 
     const rolleApi: RolleApi = constructRolleApi(page);
     const response: ApiResponse<ServiceProviderResponse[]> = await rolleApi.rolleControllerUpdateServiceProvidersByIdRaw(requestParameters);
-    await expect(response.raw.status).toBe(200);
+    await expect(response.raw.status).toBe(201);
 
     const addedServiceProviders: ServiceProviderResponse[] = await response.value();
     expect(addedServiceProviders.length).toBe(serviceProviderIds.length);
