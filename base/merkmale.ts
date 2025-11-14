cee0c3c23a7892a378a4738d1e3b86bca7d90d88
Yannick Clausen <yannick.clausen@capgemini.com>
2025-11-14 10:51:46 +0100

    use different syntax

--- base/merkmale.ts
+++ base/merkmale.ts
@@ -4,6 +4,6 @@ export const befristungPflicht: RollenMerkmal = 'BEFRISTUNG_PFLICHT';
 export const kopersNrPflicht: RollenMerkmal = 'KOPERS_PFLICHT';
 
 export const rollenMerkmalLabel: Record<RollenMerkmal, string> = {
-    'BEFRISTUNG_PFLICHT': 'Befristung ist Pflichtangabe',
-    'KOPERS_PFLICHT': 'KoPers.-Nr. ist Pflichtangabe',
+    BEFRISTUNG_PFLICHT: 'Befristung ist Pflichtangabe',
+    KOPERS_PFLICHT: 'KoPers.-Nr. ist Pflichtangabe',
 };
