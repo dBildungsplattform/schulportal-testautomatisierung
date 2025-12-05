9f6edcde661060adbd28f6ad8dc1cfa125c845b4
Yannick Clausen <yannick.clausen@capgemini.com>
2025-12-05 13:23:09 +0100

    refactor config for timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ dotenv.config({ path: path.resolve(dirname, '.env'), override: true });
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 2000,
+  timeout: 60 * 1000,
   expect: { timeout: 10000 },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
