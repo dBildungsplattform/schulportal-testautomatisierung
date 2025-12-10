d1fe6093a920dc0ec28d8b971ca13179d1816238
gebersem <emily.gebers@dataport.de>
2025-12-10 08:29:37 +0100

    Improvement: KlasseBearbeiten: Tests in for loop

--- tests/klassen/KlasseBearbeiten.spec.ts
+++ tests/klassen/KlasseBearbeiten.spec.ts
@@ -24,122 +24,65 @@ let klasseErgebnislistePage : KlasseManagementViewPage;
 let klasseDetailsPage : KlasseDetailsViewPage;
 let klasseParams : KlasseCreationParams;
 let klasseParamsBearbeitet : KlasseCreationParams;
-let landesadmin: UserInfo;
-let schuladmin: UserInfo;
+let admin: UserInfo;
 
-/*
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Landesadmin xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-*/
-test.describe(`Testfälle für das Bearbeiten von Klassen als Landesadmin: Umgebung: ${process.env.ENV}: URL: ${process.env.FRONTEND_URL}:`, () => {
-  test.beforeEach(async ({ page }: PlaywrightTestArgs) => {
-    header = new HeaderPage(page);
-    loginPage = await freshLoginPage(page);
-    await loginPage.login(process.env.USER, process.env.PW);
+[
+  { organisationsName: landSH, rolleName: landesadminRolle, bezeichnung: 'Landesadmin' },
+  { organisationsName: testschuleName, rolleName: schuladminOeffentlichRolle, bezeichnung: 'Schuladmin' },
+].forEach(({ organisationsName, rolleName, bezeichnung }: { organisationsName: string; rolleName: string; bezeichnung: string }) => {
+  test.describe(`Testfälle für das Bearbeiten von Klassen als ${bezeichnung}: Umgebung: ${process.env.ENV}: URL: ${process.env.FRONTEND_URL}:`, () => {
+    test.beforeEach(async ({ page }: PlaywrightTestArgs) => {
+      header = new HeaderPage(page);
+      loginPage = await freshLoginPage(page);
+      await loginPage.login(process.env.USER, process.env.PW);
 
-    landesadmin = await createPersonWithPersonenkontext(page, landSH, landesadminRolle);
+      admin = await createPersonWithPersonenkontext(page, organisationsName, rolleName);
 
-    landingPage = await header.logout();
-    landingPage.navigateToLogin();
+      landingPage = await header.logout();
+      landingPage.navigateToLogin();
 
-    // Erstmalige Anmeldung mit Passwortänderung
-    const startPage: StartViewPage = await loginPage.loginNewUserWithPasswordChange(landesadmin.username, landesadmin.password)
-    await startPage.waitForPageLoad();
+      // Erstmalige Anmeldung mit Passwortänderung
+      const startPage: StartViewPage = await loginPage.loginNewUserWithPasswordChange(admin.username, admin.password)
+      await startPage.waitForPageLoad();
 
-    // Navigation zur Klassenanlage
-    personManagementViewPage = await startPage.goToAdministration();
-    klasseAnlegenPage = await personManagementViewPage.menu.navigateToKlasseCreation(); 
+      // Navigation zur Klassenanlage
+      personManagementViewPage = await startPage.goToAdministration();
+      klasseAnlegenPage = await personManagementViewPage.menu.navigateToKlasseCreation(); 
 
-    // Testdaten vorbereiten
-    klasseParams = {
-      schulname: testschuleName,
-      klassenname: await generateKlassenname()
-    };
+      // Testdaten vorbereiten
+      klasseParams = {
+        schulname: testschuleName,
+        klassenname: await generateKlassenname(),
+        schulNr: testschuleDstNr
+      };
 
-    klasseParamsBearbeitet = {
-      schulname: testschuleName,
-      klassenname: await generateKlassenname()
-    };
+      klasseParamsBearbeitet = {
+        schulname: testschuleName,
+        klassenname: await generateKlassenname(),
+        schulNr: testschuleDstNr
+      };
+    });
+
+    // SPSH-2856
+    test(`Klasse bearbeiten als ${bezeichnung}`, { tag: [LONG, SHORT, STAGE, BROWSER] },  async () => {
+      await test.step(`Klasse anlegen`, async () => {
+        klasseErfolgreichAngelegtPage = await klasseAnlegenPage.createKlasse(rolleName == landesadminRolle, klasseParams);
+        await klasseErfolgreichAngelegtPage.waitForPageLoad();
+        await klasseErfolgreichAngelegtPage.checkSuccessPage(klasseParams);
+      });
+
+      await test.step(`Klasse öffnen`, async () => {
+        klasseErgebnislistePage = await klasseErfolgreichAngelegtPage.goBackToList();
+        await klasseErgebnislistePage.waitForPageLoad();
+        klasseDetailsPage = await klasseErgebnislistePage.searchAndOpenGesamtuebersicht(rolleName == landesadminRolle, klasseParams);
+      });
+
+      await test.step(`Klasse bearbeiten und Bestätigungsseite prüfen`, async () => {
+        await klasseDetailsPage.checkDetailsForm();
+        await klasseDetailsPage.editKlasse(klasseParamsBearbeitet.klassenname);
+        await klasseDetailsPage.klasseSuccessfullyEdited(klasseParamsBearbeitet.schulname, testschuleDstNr, klasseParamsBearbeitet.klassenname);
+      });
+
+    });
   });
-
-  // SPSH-2856
-  test('Klasse bearbeiten als Landesadmin', { tag: [LONG, SHORT, STAGE, BROWSER] },  async () => {
-    await test.step(`Klasse anlegen`, async () => {
-      klasseErfolgreichAngelegtPage = await klasseAnlegenPage.createKlasse(true, klasseParams);
-      await klasseErfolgreichAngelegtPage.waitForPageLoad();
-      await klasseErfolgreichAngelegtPage.checkSuccessPage(klasseParams);
-    });
-
-    await test.step(`Klasse öffnen`, async () => {
-      klasseErgebnislistePage = await klasseErfolgreichAngelegtPage.goBackToList();
-      await klasseErgebnislistePage.waitForPageLoad();
-      klasseDetailsPage = await klasseErgebnislistePage.searchAndOpenGesamtuebersicht(true, klasseParams);
-    });
-
-    await test.step(`Klasse bearbeiten und Bestätigungsseite prüfen`, async () => {
-      await klasseDetailsPage.checkDetailsForm();
-      await klasseDetailsPage.editKlasse(klasseParamsBearbeitet.klassenname);
-      await klasseDetailsPage.klasseSuccessfullyEdited(klasseParamsBearbeitet.schulname, testschuleDstNr, klasseParamsBearbeitet.klassenname);
-    });
-
-  });
-});
-
-/*
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx Schuladmin xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-*/
-test.describe(`Testfälle für das Bearbeiten von Klassen als Schuladmin: Umgebung: ${process.env.ENV}: URL: ${process.env.FRONTEND_URL}:`, () => {
-  test.beforeEach(async ({ page }: PlaywrightTestArgs) => {
-
-    header = new HeaderPage(page);
-    loginPage = await freshLoginPage(page);
-    await loginPage.login(process.env.USER, process.env.PW);
-
-    schuladmin = await createPersonWithPersonenkontext(page, testschuleName, schuladminOeffentlichRolle);
-
-    landingPage = await header.logout();
-    landingPage.navigateToLogin();
-
-    // Erstmalige Anmeldung mit Passwortänderung
-    const startPage: StartViewPage = await loginPage.loginNewUserWithPasswordChange(schuladmin.username, schuladmin.password)
-    await startPage.waitForPageLoad();
-
-    personManagementViewPage = await startPage.goToAdministration();
-    klasseAnlegenPage = await personManagementViewPage.menu.navigateToKlasseCreation();
-
-    // Testdaten vorbereiten
-    klasseParams = {
-      schulname: testschuleName,
-      klassenname: await generateKlassenname(),
-      schulNr: testschuleDstNr
-    };
-
-    klasseParamsBearbeitet = {
-      schulname: testschuleName,
-      klassenname: await generateKlassenname(),
-      schulNr: testschuleDstNr
-    };
-  });
-
-  // SPSH-2857
-  test('Klasse bearbeiten als Schuladmin', { tag: [LONG, SHORT, STAGE, BROWSER] },  async () => {
-    await test.step(`Klasse anlegen`, async () => {
-      klasseErfolgreichAngelegtPage = await klasseAnlegenPage.createKlasse(false, klasseParams);
-      await klasseErfolgreichAngelegtPage.waitForPageLoad();
-      await klasseErfolgreichAngelegtPage.checkSuccessPage(klasseParams);
-    });
-
-    await test.step(`Klasse öffnen`, async () => {
-      klasseErgebnislistePage = await klasseErfolgreichAngelegtPage.goBackToList();
-      await klasseErgebnislistePage.waitForPageLoad();
-      klasseDetailsPage = await klasseErgebnislistePage.searchAndOpenGesamtuebersicht(false, klasseParams);
-    });
-
-    await test.step(`Klasse bearbeiten und Bestätigungsseite prüfen`, async () => {
-      await klasseDetailsPage.checkDetailsForm();
-      await klasseDetailsPage.editKlasse(klasseParamsBearbeitet.klassenname);
-      await klasseDetailsPage.klasseSuccessfullyEdited(klasseParamsBearbeitet.schulname, testschuleDstNr, klasseParamsBearbeitet.klassenname);
-    });
-
-  });
-
 });
