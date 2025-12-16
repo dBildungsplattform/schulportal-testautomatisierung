262c957991d5c6dff4c36ce206dba644ad345d0e
gebersem <emily.gebers@dataport.de>
2025-12-16 14:19:59 +0100

    Schule-Klasse in Ordner verschoben

--- tests/schulen/Schule.spec.ts
+++ tests/schulen/Schule.spec.ts
@@ -1,15 +1,15 @@
 import { expect, PlaywrightTestArgs, test } from '@playwright/test';
-import { BROWSER, DEV, LONG, SHORT } from '../base/tags';
-import { generateDienststellenNr, generateSchulname } from '../base/utils/generateTestdata';
-import FromAnywhere from '../pages/FromAnywhere';
-import { LandingPage } from '../pages/LandingView.page';
-import { LoginPage } from '../pages/LoginView.page';
-import { StartPage } from '../pages/StartView.page';
-import { SchuleCreationViewPage } from '../pages/admin/organisationen/schulen/SchuleCreationView.page';
-import { SchuleManagementViewPage } from '../pages/admin/organisationen/schulen/SchuleManagementView.page';
-import { FooterDataTablePage } from '../pages/components/FooterDataTable.page';
-import { HeaderPage } from '../pages/components/Header.page';
-import { MenuPage } from '../pages/components/MenuBar.page';
+import { BROWSER, DEV, LONG, SHORT } from '../../base/tags';
+import { generateDienststellenNr, generateSchulname } from '../../base/utils/generateTestdata';
+import FromAnywhere from '../../pages/FromAnywhere';
+import { LandingPage } from '../../pages/LandingView.page';
+import { LoginPage } from '../../pages/LoginView.page';
+import { StartPage } from '../../pages/StartView.page';
+import { SchuleCreationViewPage } from '../../pages/admin/organisationen/schulen/SchuleCreationView.page';
+import { SchuleManagementViewPage } from '../../pages/admin/organisationen/schulen/SchuleManagementView.page';
+import { FooterDataTablePage } from '../../pages/components/FooterDataTable.page';
+import { HeaderPage } from '../../pages/components/Header.page';
+import { MenuPage } from '../../pages/components/MenuBar.page';
 
 let logoutViaStartPage: boolean = false;
 
