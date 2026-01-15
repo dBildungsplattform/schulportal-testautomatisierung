3868e13ffa89e38a1e94bdc15e1ae6dac1dff6d8
godtierbatuhan <boucharay1001@gmail.com>
2026-01-15 11:04:53 +0100

    headless false

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     timezoneId: 'Europe/Brussels',
     screenshot: 'only-on-failure',
     baseURL: FRONTEND_URL,
-    headless: false,
+    headless: true,
   },
 
   projects: [
