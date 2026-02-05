a7b5b03c6ba2bb7730e123de8e4a1800101e5a61
Yannick Clausen <yannick.clausen@capgemini.com>
2026-02-05 17:43:27 +0100

    refactor: navigation

--- pages/FromAnywhere.neu.ts
+++ pages/FromAnywhere.neu.ts
@@ -4,8 +4,15 @@ import { LandingViewPage } from './LandingView.neu.page';
 function FromAnywhere(page: Page): { start: () => Promise<LandingViewPage> } {
   return {
     async start(): Promise<LandingViewPage> {
-      await page.goto('/');
-      return new LandingViewPage(page);
+      await page.goto('/', { waitUntil: 'load' });
+      try {
+        const landingPage: LandingViewPage = await new LandingViewPage(page).waitForPageLoad();
+        return landingPage;
+      } catch (error) {
+        console.log('Reloading page due to error in loading landing page:', error);
+        await page.reload();
+        return new LandingViewPage(page).waitForPageLoad();
+      }
     },
   };
 }
