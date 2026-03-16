13a6d539be94abbbbae23510abaa19cc656feecf
ChantalleCap <silvia.grosche@capgemini.com>
2026-03-16 06:28:21 +0100

    change assertion-names

--- pages/StartView.neu.page.ts
+++ pages/StartView.neu.page.ts
@@ -23,7 +23,7 @@ export class StartViewPage {
   }
 
   /* assertions */
-  public async serviceProvidersAreLoaded(): Promise<void> {
+  public async assertServiceProvidersAreLoaded(): Promise<void> {
     await this.page.waitForResponse(
       (response: Response) => response.url().includes('/api/provider') && response.status() === 200
     );
@@ -31,7 +31,7 @@ export class StartViewPage {
     await expect(this.page.getByTestId('all-service-provider-title')).toBeVisible();
   }
 
-  public async serviceProvidersAreVisible(serviceProviderNames: string[]): Promise<void> {
+  public async assertServiceProvidersAreVisible(serviceProviderNames: string[]): Promise<void> {
     await Promise.all(
       serviceProviderNames.map(async (serviceProviderName: string) => {
         const serviceProviderCard: Locator = this.page.locator(`[data-testid^="service-provider-card"]`, {
@@ -43,7 +43,7 @@ export class StartViewPage {
     );
   }
 
-  public async checkSpIsHidden(spNames: string[]): Promise<void> {
+  public async assertServiceProviderIsHidden(spNames: string[]): Promise<void> {
     for (const spName of spNames) {
       await expect(this.page.locator('[data-testid^="service-provider-card"]', { hasText: spName })).toBeHidden();
     }
