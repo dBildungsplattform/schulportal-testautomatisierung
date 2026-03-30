7455326cbc5c59a04455b89293ce45b2628ab609
Yannick Clausen <yannick.clausen@capgemini.com>
2026-03-30 13:47:00 +0200

    feat: add tags

--- tests/start/ServiceProviderAufStartseite.spec.ts
+++ tests/start/ServiceProviderAufStartseite.spec.ts
@@ -31,6 +31,7 @@ import { LandingViewPage } from '../../pages/LandingView.neu.page';
 import { LoginViewPage } from '../../pages/LoginView.neu.page';
 import { StartViewPage } from '../../pages/StartView.neu.page';
 import { testFixtures } from './ServiceProviderAufStartseite.data';
+import { DEV, STAGE } from '../../base/tags';
 
 const allProviderNames: string[] = [
   email,
@@ -49,38 +50,40 @@ const allProviderNames: string[] = [
 
 test.describe('ServiceProvider auf Startseite', () => {
   for (const { rollenArt, serviceProviderNames } of testFixtures) {
-    test(`Als ${rollenArt} prüfen, dass die richtigen ServiceProvider sichtbar sind`, async ({
-      page,
-    }: PlaywrightTestArgs) => {
-      const startPage: StartViewPage = await test.step('Daten anlegen', async () => {
-        const personManagementViewPage: PersonManagementViewPage = await loginAndNavigateToAdministration(page);
-        const schulName: string = generateSchulname();
-        const schuleId: string = await createSchule(page, schulName);
-        const klasseId: string | null =
-          rollenArt === RollenArt.Lern ? await createKlasse(page, schuleId, generateKlassenname()) : null;
+    test(
+      `Als ${rollenArt} prüfen, dass die richtigen ServiceProvider sichtbar sind`,
+      { tag: [STAGE, DEV] },
+      async ({ page }: PlaywrightTestArgs) => {
+        const startPage: StartViewPage = await test.step('Daten anlegen', async () => {
+          const personManagementViewPage: PersonManagementViewPage = await loginAndNavigateToAdministration(page);
+          const schulName: string = generateSchulname();
+          const schuleId: string = await createSchule(page, schulName);
+          const klasseId: string | null =
+            rollenArt === RollenArt.Lern ? await createKlasse(page, schuleId, generateKlassenname()) : null;
 
-        const userInfo: UserInfo = await createRolleAndPersonWithPersonenkontext(
-          page,
-          schulName,
-          rollenArt,
-          generateNachname(),
-          generateVorname(),
-          await Promise.all(serviceProviderNames.map((name: string) => getServiceProviderId(page, name))),
-          generateRolleName(),
-          undefined,
-          klasseId ? klasseId : undefined,
-        );
-        const landingViewPage: LandingViewPage = await personManagementViewPage.getHeader().logout();
-        const loginPage: LoginViewPage = await landingViewPage.navigateToLogin();
-        return await loginPage.loginNewUserWithPasswordChange(userInfo.username, userInfo.password);
-      });
-      await test.step('Sichtbarkeit der ServiceProvider prüfen', async () => {
-        await startPage.waitForPageLoad();
-        await startPage.assertServiceProvidersAreVisible(serviceProviderNames);
-        await startPage.assertServiceProvidersAreHidden(
-          allProviderNames.filter((name: string) => !serviceProviderNames.includes(name)),
-        );
-      });
-    });
+          const userInfo: UserInfo = await createRolleAndPersonWithPersonenkontext(
+            page,
+            schulName,
+            rollenArt,
+            generateNachname(),
+            generateVorname(),
+            await Promise.all(serviceProviderNames.map((name: string) => getServiceProviderId(page, name))),
+            generateRolleName(),
+            undefined,
+            klasseId ? klasseId : undefined,
+          );
+          const landingViewPage: LandingViewPage = await personManagementViewPage.getHeader().logout();
+          const loginPage: LoginViewPage = await landingViewPage.navigateToLogin();
+          return await loginPage.loginNewUserWithPasswordChange(userInfo.username, userInfo.password);
+        });
+        await test.step('Sichtbarkeit der ServiceProvider prüfen', async () => {
+          await startPage.waitForPageLoad();
+          await startPage.assertServiceProvidersAreVisible(serviceProviderNames);
+          await startPage.assertServiceProvidersAreHidden(
+            allProviderNames.filter((name: string) => !serviceProviderNames.includes(name)),
+          );
+        });
+      },
+    );
   }
 });
