46556e5bd6c8ff5014718a3a2d3f2538141dc2e8
ChantalleCap <silvia.grosche@capgemini.com>
2026-05-20 07:32:28 +0200

    neue schulen eingetragen

--- base/organisation.ts
+++ base/organisation.ts
@@ -1,10 +1,10 @@
-export const testschuleName: string = 'Testschule Schulportal';
-export const testschuleDstNr: string = '1111111';
-export const testschuleDstNrUndName: string = '1111111 (Testschule Schulportal)';
-export const testschule665Name: string = 'Testschule-PW665';
-export const testschule665DstNr: string = '1111165';
-export const testschule665DstNrUndName: string = '1111165 (Testschule-PW665)';
-export const ersatzTestschuleName: string = 'Duborg-Skolen';
+export const testschuleName: string = 'Playwrightschule';
+export const testschuleDstNr: string = '1111138';
+export const testschuleDstNrUndName: string = '1111138 (Playwrightschule)';
+export const testschule665Name: string = 'Playwright Schule';
+export const testschule665DstNr: string = '1111139';
+export const testschule665DstNrUndName: string = '1111139 (Playwright Schule)';
+export const ersatzTestschuleName: string = 'Duborg-Skolen Ersatzschule';
 export const ersatzTestschuleDstNr: string = '0799801';
 export const landSH: string = 'Land Schleswig-Holstein';
 export const ersatzLandSH: string = 'Ersatzschulen Land Schleswig-Holstein';
