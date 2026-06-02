2e49f28e6c36a92462b69561f74eca8c3c11592f
godtierbatuhan <boucharay1001@gmail.com>
2026-06-02 14:03:28 +0200

    skip anlage tests

--- tests/personen/PersonenErgebnislisteDurchsuchen.spec.ts
+++ tests/personen/PersonenErgebnislisteDurchsuchen.spec.ts
@@ -51,7 +51,8 @@ interface AdminFixture {
   let schuleId2: string;
   let schuleParams: SchuleCreationParams;
 
-  test.describe(`Testfälle für die Ergebnisliste von Benutzern als ${bezeichnung}: Umgebung: ${process.env.ENV}: URL: ${process.env.FRONTEND_URL}:`, () => {
+  test.describe
+    .skip(`Testfälle für die Ergebnisliste von Benutzern als ${bezeichnung}: Umgebung: ${process.env.ENV}: URL: ${process.env.FRONTEND_URL}:`, () => {
     test.beforeEach(async ({ page }: PlaywrightTestArgs) => {
       header = new HeaderPage(page);
       personManagementViewPage = await loginAndNavigateToAdministration(page);
