3f0712abad684c8e6d56ac9c337c1ea4a4358303
godtierbatuhan <boucharay1001@gmail.com>
2026-06-02 16:45:12 +0200

    Fixed prefix

--- base/utils/generateTestdata.ts
+++ base/utils/generateTestdata.ts
@@ -7,7 +7,7 @@ const shardLetter = String.fromCharCode(65 + parseInt(shardIndex, 10)); // 0→A
 
 export function generateVorname(): string {
   return (
-    `TAuto-PW-V${shardLetter}-` +
+    `TAuto-PW-S${shardLetter}-` +
     faker.person.firstName() +
     generateRandomString({ length: 3, charSetType: CharacterSetType.Alphabetic })
   );
@@ -15,7 +15,7 @@ export function generateVorname(): string {
 
 export function generateNachname(): string {
   return (
-    `TAuto-PW-N${shardLetter}-` +
+    `TAuto-PW-S${shardLetter}-` +
     faker.person.lastName() +
     generateRandomString({ length: 3, charSetType: CharacterSetType.Alphabetic })
   );
