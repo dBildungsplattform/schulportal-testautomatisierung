ae87d87c818ce2a7d8c4fe02c125a6433c12e428
Yannick Clausen <yannick.clausen@capgemini.com>
2026-07-03 14:12:48 +0200

    refactor: remove unused fn, add doc

--- base/utils/conversion.ts
+++ base/utils/conversion.ts
@@ -1,7 +1,8 @@
-export function stringToBoolean(value: string): boolean {
-  return value.toLowerCase() === 'ja';
-}
-
+/**
+ * Maps a boolean to 'Ja' or 'Nein'.
+ * @param value The boolean to convert.
+ * @returns 'Ja' if the boolean is true, 'Nein' otherwise.
+ */
 export function booleanToString(value: boolean): string {
   return value ? 'Ja' : 'Nein';
 }
