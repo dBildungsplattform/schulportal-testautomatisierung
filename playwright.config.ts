73375b4edfb2c4817154606b98565a2ea318f071
godtierbatuhan <boucharay1001@gmail.com>
2026-07-19 16:52:46 +0200

    All browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     timezoneId: 'Europe/Brussels',
     screenshot: 'only-on-failure',
     baseURL: FRONTEND_URL,
-    headless: true,
+    headless: false,
   },
 
   projects: [
@@ -46,6 +46,41 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
         viewport: { width: 1920, height: 1080 },
         ignoreHTTPSErrors: true,
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1920, height: 1080 },
+        ignoreHTTPSErrors: true,
+      },
+    },
+    {
+      name: 'webkit',
+      timeout: 150 * 1000,
+      use: {
+        ...devices['Desktop Safari'],
+        viewport: { width: 1920, height: 1080 },
+        ignoreHTTPSErrors: true,
+        contextOptions: {
+          reducedMotion: 'reduce',
+        },
+      },
+    },
+    /* Test against branded browsers. */
+    {
+      name: 'msedge',
+      use: {
+        channel: 'msedge',
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: 'chrome',
+      use: {
+        channel: 'chrome',
+        viewport: { width: 1920, height: 1080 },
       },
     },
 
